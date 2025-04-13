85d18f41a252efa682e2c4bdead38578c6725bf7
Nafees Iqbal <iqbalnafee@gmail.com>
2025-04-13 13:05:02 +0200

    kotlin variable declaration defined

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+package org.example
+
+fun main() {
+    println("Hello World!")
+    var name = "John"
+    var year = 2025
+     //    The difference between var and val is that
+    //    variables declared with the var keyword can be changed/modified, while val variables cannot.
+}
