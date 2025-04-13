24fdce39019e48ad7ffb2f17a8dae9dc0c52d104
Nafees Iqbal <iqbalnafee@gmail.com>
2025-04-13 13:11:39 +0200

    declare variable without value

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,18 @@
 package org.example
 
 fun main() {
-    println("Hello World!")
     var name = "John"
     var year = 2025
+
      //    The difference between var and val is that
     //    variables declared with the var keyword can be changed/modified, while val variables cannot.
+
+
+    /* we can also declare a variable without assigning the value,
+    and assign the value later. However, this is only possible when you specify the type:*/
+    var myName: String
+    myName = "Nafees"
+
+    println("Hello " + myName)
+    println("Hello $myName")
 }
