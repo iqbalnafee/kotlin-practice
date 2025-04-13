59aa3f4b1b7b6808028495a5e46c66dbba3e1e28
Nafees Iqbal <iqbalnafee@gmail.com>
2025-04-13 13:37:00 +0200

    functions done

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,15 @@
 package org.example
 
+//you must specify the type of each parameter
+fun myFunc(myName: String): String {
+    return "Hello my name is $myName"
+}
+
 fun main() {
     var name = "John"
     var year = 2025
 
-     //    The difference between var and val is that
+    //    The difference between var and val is that
     //    variables declared with the var keyword can be changed/modified, while val variables cannot.
 
 
@@ -43,9 +48,9 @@ fun main() {
 
     /*Unlike Java and other programming languages, there is no traditional for loop in Kotlin.*/
     val nums = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    for(num in nums)
+    for (num in nums)
         println(num)
 
-    for(char in 'a'..'x')
+    for (char in 'a'..'x')
         println(char)
 }
