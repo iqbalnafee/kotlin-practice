76db99c66725c4e763695ac438549de8de38b818
iqbalnafee <iqbalnafee@gmail.com>
2025-10-18 17:13:56 +0200

    method overriding

--- src/main/kotlin/ClassWithGetter.kt
+++ src/main/kotlin/ClassWithGetter.kt
@@ -1,7 +1,8 @@
 fun main(){
     try{
-      val s1: Shape = Shape(-22.00, 25.00)
-      println(s1.getWidth)
+      val s1: Shape = Shape(22.00, 25.00)
+      val s2: Shape = Shape(2.00, 25.00)
+      println(s1.equals(s2))
     }
     catch(e: IllegalArgumentException){ // cannot write catch(val e: Exception), because catch block in Kotlin uses a parameter, not a property declaration
         println(e.message)
@@ -19,5 +20,11 @@ class Shape(private val width: Double, private val height: Double){
         require(width>0) {
             "width must be greater than 0"
         }
+    }
+    
+    override fun equals(other: Any?): Boolean
+    {
+        if(other is Shape) if(other.width == this.width) return true
+        return false
     }
 }
