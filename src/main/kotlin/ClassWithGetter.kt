4f186e9e281a444656a3c82f67643de467e3d14c
iqbalnafee <iqbalnafee@gmail.com>
2025-10-18 14:24:27 +0200

    class getter add

---
+++ src/main/kotlin/ClassWithGetter.kt
@@ -0,0 +1,12 @@
+fun main(){
+    val s1: Shape = Shape(22.00, 25.00)
+    println(s1.getWidth)
+}
+class Shape(private val width: Double, private val height: Double){
+
+    val getWidth: Double
+        get() = width
+    
+    val getHeight: Double
+        get() = height
+}
