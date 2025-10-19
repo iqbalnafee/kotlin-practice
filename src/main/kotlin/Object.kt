2b80725e2bb6df51e6c6ced186195c7ba144f819
iqbalnafee <iqbalnafee@gmail.com>
2025-10-20 01:28:41 +0200

    object implementation done

--- src/main/kotlin/Object.kt
+++ src/main/kotlin/Object.kt
@@ -2,10 +2,20 @@ fun main(){
 
     // to call singleton object
     Database.connect()
+
+    val listener: ButtonClickListener = object :ButtonClickListener(){
+        override fun onClick(){
+            println("Button Clicked from listener")
+        }
+    }
 }
 
 // for singleton pattern
 
 object Database{ //this is not class, this a single object of database
     fun connect() = println("connected to database")
+}
+
+abstract class ButtonClickListener {
+    abstract fun onClick()
 }
